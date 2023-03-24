dd461226160ed2cebb0f87116ef202012aba3a58
gonixxxx <akdntm946@gmail.com>
2023-03-24 16:19:36 +0900

    dgo

--- src/main/java/com/smhrd/model/BoardDAO.java
+++ src/main/java/com/smhrd/model/BoardDAO.java
@@ -1,5 +1,8 @@
 package com.smhrd.model;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import com.smhrd.db.SqlSessionManager;
@@ -8,4 +11,18 @@ public class BoardDAO {
 
 	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
 	
+	public ArrayList<BoardVO> showReview(BoardVO vo) {
+		SqlSession sqlSession =  sqlSessionFactory.openSession(true);
+		ArrayList<BoardVO> review_list = (ArrayList)sqlSession.selectList("showReview", vo);
+		sqlSession.close();
+		
+		return review_list;
+	}
+	
+	public int insertReview(BoardVO vo) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		int cnt = sqlSession.insert("insertReview", vo);
+		
+		return cnt;
+	}
 }
--- src/main/java/com/smhrd/model/BoardVO.java
+++ src/main/java/com/smhrd/model/BoardVO.java
@@ -1,5 +1,37 @@
 package com.smhrd.model;
 
 public class BoardVO {
-
+	private int r_num;
+	private String movie_id;
+	private String member_id;
+	private String like_check;
+	private String member_rate;
+	
+	
+	public BoardVO(int r_num, String movie_id, String member_id, String like_check, String member_rate) {
+		super();
+		this.r_num = r_num;
+		this.movie_id = movie_id;
+		this.member_id = member_id;
+		this.like_check = like_check;
+		this.member_rate = member_rate;
+	}
+	
+	public int getR_num() {
+		return r_num;
+	}
+	public String getMovie_id() {
+		return movie_id;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public String getLike_check() {
+		return like_check;
+	}
+	public String getMember_rate() {
+		return member_rate;
+	}
+	
+	
 }
